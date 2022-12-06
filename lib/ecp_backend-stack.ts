2d8adf0457c40db8dcfabbe380e9ecbb4fafbbc7
204078A <204078A@mymail.nyp.edu.sg>
2022-12-07 03:18:59 +0800

    chore: add rds & dynamodb to stack

--- lib/ecp_backend-stack.ts
+++ lib/ecp_backend-stack.ts
@@ -2,7 +2,8 @@ import * as cdk from "aws-cdk-lib";
 import { Construct } from 'constructs';
 import * as s3 from "aws-cdk-lib/aws-s3";
 import * as rds from "aws-cdk-lib/aws-rds";
-import * as ec2 from "aws-cdk-lib/aws-ec2"
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
 import { LambdaRestApi } from "aws-cdk-lib/aws-apigateway";
 
 export class EcpBackendStack extends cdk.Stack {
@@ -12,8 +13,9 @@ export class EcpBackendStack extends cdk.Stack {
     private bucket: s3.Bucket;
     // private rds_ha: rds.DatabaseCluster; // High availability
     private rds_la: rds.DatabaseInstance; // Low availability
+    private dynamo: dynamodb.Table;
 
-    constructor(scope: Construct, id: string, props?: StackProps) {
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
         // The code that defines your stack goes here
@@ -45,5 +47,20 @@ export class EcpBackendStack extends cdk.Stack {
                 }
             ]
         })
+
+        this.rds_la = new rds.DatabaseInstance(this, "jobeasy-db", {
+            engine: rds.DatabaseInstanceEngine.mariaDb({
+                version: rds.MariaDbEngineVersion.VER_10_6,
+            }),
+            instanceType: ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE3, ec2.InstanceSize.MICRO),
+            vpc: this.vpc,
+            vpcSubnets: {
+                subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS
+            }
+        })
+
+        this.dynamo = new dynamodb.Table(this, 'Table', {
+            partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+        });
     }
 }
